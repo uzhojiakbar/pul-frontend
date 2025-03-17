dddec57d39533c2b48c9735fd7f588386ef5fcf1
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-03-17 09:19:27 +0500

    server changed

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:5000/",
-        target: "https://pul-backend.onrender.com/",
+        target: "http://37.27.215.130:5020/",
+        // target: "https://pul-backend.onrender.com/",
 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
